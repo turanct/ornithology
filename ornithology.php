62c6c220e9fa761255cd6ef99d61d7aba612ee2c
turanct <spinnewebber_toon@hotmail.com>
2014-06-07 13:14:55 +0200

    Make the application executable
    Rookie mistake :-)

--- ornithology.php
+++ ornithology.php
@@ -11,7 +11,7 @@ $dataStore = new Ornithology\Service\DataStoreFile(getenv('HOME') . '/.ornitholo
 $twitterService = new Ornithology\Service\Twitter($dataStore);
 
 // Initialize the application
-$application = new Console\Application('Ornithology', '0.0.1');
+$application = new Console\Application('Ornithology', '0.0.2');
 
 // Add application related commands
 $application->add(new Ornithology\Command\Quit('exit'));
